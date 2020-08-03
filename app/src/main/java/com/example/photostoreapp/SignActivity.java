34795ae4125bbe3ca7f27acb7c5d5f097bcd7455
Ramim <ramimhridoy512@gmail.com>
2020-08-04 01:03:37 +0600

    file2 added and firebase conected

--- app/src/main/java/com/example/photostoreapp/SignActivity.java
+++ app/src/main/java/com/example/photostoreapp/SignActivity.java
@@ -1,14 +1,132 @@
 package com.example.photostoreapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    private EditText signInEmailEditText,signInPasswordEditText;
+    private TextView signUpTextView;
+    private Button signInButton;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+    private  TextView text2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
+
+    RelativeLayout relativeLayout = findViewById(R.id.layoutID);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(100);
+        animationDrawable.setExitFadeDuration(100);
+        animationDrawable.start();
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        this.setTitle("Sign In Activity");
+
+        signInEmailEditText = findViewById(R.id.signInEmailEditTextID);
+        signInPasswordEditText = findViewById(R.id.signInpasswordEditTextID);
+
+        signUpTextView = findViewById(R.id.signUpTextViewID);
+        signInButton = findViewById(R.id.signInButtonID);
+        progressBar = findViewById(R.id.progressbarID);
+        text2 = findViewById(R.id.back2);
+
+
+        signUpTextView.setOnClickListener(this);
+        signInButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.signInButtonID:
+
+                userLogin();
+                break;
+
+            case  R.id.signUpTextViewID:
+
+                Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+
+    }
+
+
+
+    private void userLogin() {
+
+        String email = signInEmailEditText.getText().toString().trim();
+        String password = signInPasswordEditText.getText().toString().trim();
+
+        //checking the validity of the email
+        if (email.isEmpty()) {
+            signInEmailEditText.setError("Enter an email address");
+            signInEmailEditText.requestFocus();
+            return;
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            signInEmailEditText.setError("Enter a valid email address");
+            signInEmailEditText.requestFocus();
+            return;
+        }
+
+        //checking the validity of the password
+        if (email.isEmpty()) {
+            signInPasswordEditText.setError("Enter a password");
+            signInPasswordEditText.requestFocus();
+            return;
+        }
+
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()){
+                    finish();
+                    Intent in = new Intent(getApplicationContext(),HomeActivity.class);
+                    in.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(in);
+                }
+                else {
+
+                    Toast.makeText(getApplicationContext(),"Login Unsuccessful", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
     }
 }
